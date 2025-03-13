d22de51afded418dd13c6fd0ff74a7cd90b9ca1d
HermenStudent <andreshermen1012@gmail.com>
2025-03-12 21:41:13 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -48,6 +48,7 @@ function getCardElement(data) {
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardNameEl = cardElement.querySelector(".card__title");
   cardImageEl.src = data.link;
+  cardImageEl.alt = data.link;
   cardNameEl.textContent = data.name;
   return cardElement;
 }
