47a0f6005c30b8828ce01f93ee71ce8143e0c992
HermenStudent <andreshermen1012@gmail.com>
2025-03-28 18:07:42 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -47,6 +47,9 @@ const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
+const previewModalCloseBtn = previewModal.querySelector(
+  ".modal__close-btn_type_preview"
+);
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
@@ -65,11 +68,6 @@ function handleAddCardSubmit(evt) {
   cardsList.prepend(cardElement);
   closeModal(cardModal);
   evt.target.reset();
-}
-
-function handleDeleteCardSubmit(evt) {
-  evt.preventDefault();
-  const cardElement = remove(inputValues);
 }
 
 function getCardElement(data) {
@@ -130,6 +128,10 @@ cardModalBtn.addEventListener("click", () => {
 
 cardModalCloseBtn.addEventListener("click", () => {
   closeModal(cardModal);
+});
+
+previewModalCloseBtn.addEventListener("click", () => {
+  closeModal(previewModal);
 });
 
 editFormElement.addEventListener("submit", handleEditFormSubmit);
